ca2b5e7109e7d6a103fc4d72ce8fb6064fb59e73
DanPrOgRaMaCaO <dan75yz7@gmail.com>
2025-04-18 03:49:37 +0000

    SuperTrunfo completo

--- supertrunfomestre.c
+++ supertrunfomestre.c
@@ -4,7 +4,7 @@ int main() {
     char EstadoSP = 'A';
     char CodigoSP[20] = "A01";
     char NomeSP[30] = "São Paulo";
-    int PopulacaoSP = 12325000;
+    unsigned long int PopulacaoSP = 12325000;
     float AreaSP = 1521.11;
     long double PIBsp = 699000000000.28;
     int PontosTuristicosSP = 50;
@@ -12,7 +12,7 @@ int main() {
     char EstadoRJ = 'B';
     char CodigoRJ[20] = "B02";
     char NomeRJ[30] = "Rio de Janeiro";
-    int PopulacaoRJ = 6748000;
+    unsigned long int PopulacaoRJ = 6748000;
     float AreaRJ = 1200.25;
     long double PIBrj = 300000000000.50;
     int PontosTuristicosRJ = 30;
@@ -21,7 +21,7 @@ int main() {
     printf("Estado: %c\n", EstadoSP);
     printf("Código: %s\n", CodigoSP);
     printf("Nome: %s\n", NomeSP);
-    printf("População: %d de pessoas\n", PopulacaoSP);
+    printf("População: %lu de pessoas\n", PopulacaoSP);
     printf("Área (em km²): %.2f km²\n", AreaSP);    
     printf("PIB: %.2Lf R$\n", PIBsp);
     printf("Número de pontos de turísticos: %d\n", PontosTuristicosSP);
@@ -39,7 +39,7 @@ int main() {
     printf("Estado: %c\n", EstadoRJ);
     printf("Código: %s\n", CodigoRJ);
     printf("Nome: %s\n", NomeRJ);
-    printf("População: %d de pessoas\n", PopulacaoRJ);
+    printf("População: %lu de pessoas\n", PopulacaoRJ);
     printf("Área (em km²): %.2f km²\n", AreaRJ);    
     printf("PIB: %.2Lf R$\n", PIBrj);
     printf("Número de pontos de turísticos: %d\n", PontosTuristicosRJ);
@@ -53,6 +53,19 @@ int main() {
     printf("O PIB per capita do Rio de Janeiro é: %.2fR$\n", PIBpercapitaRJ);
     printf("\n");
 
+    // Comparação das cartas
+    float SuperPoderSP = PopulacaoSP + AreaSP + PIBsp + PontosTuristicosSP + PIBpercapitaSP + 1 / DensidadePopulacionalSP;
+    float SuperPoderRJ = PopulacaoRJ + AreaRJ + PIBrj + PontosTuristicosRJ + PIBpercapitaRJ + 1 / DensidadePopulacionalRJ;
+    
+    // "1" quando a carta 1 vencer/"0" quando a carta 2 vencer
+    printf("População: (%d) = Carta 1 venceu\n", (PopulacaoSP > PopulacaoRJ));
+    printf("Área (em km²): (%d) = Carta 1 venceu\n", (AreaSP > AreaRJ));
+    printf("PIB: (%d) = Carta 1 venceu\n", (PIBsp > PIBrj));
+    printf("Numero de pontos turísticos: (%d) = Carta 1 venceu\n", (PontosTuristicosSP > PontosTuristicosRJ));
+    printf("PIB percapita: (%d) = Carta 1 venceu\n", (PIBpercapitaSP > PIBpercapitaRJ));
+    printf("Densidade Populacional: (%d) = Carta 2 venceu\n", (1 / DensidadePopulacionalSP > 1 / DensidadePopulacionalRJ));
+    printf("Super poder: (%d) = Carta 1 venceu\n", (SuperPoderSP > SuperPoderRJ));
+    printf("\n");
 
     return 0;
 }
--- supertrunfonovato.c
+++ supertrunfonovato.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
- 
- int main() {
-     char Estado = 'A';
-     char Codigo[20] = "A01";
-     char Nome[30] = "São Paulo";
-     int Populacao = 12325000;
-     float Area = 1521.11;
-     double PIB = 699000000000.28;
-     int Turisticos = 50;
- 
-     char estado = 'B';
-     char codigo[20] = "B02";
-     char nome[30] = "Rio de Janeiro";
-     int populacao = 6748000;
-     float area = 1200.25;
-     double pib = 300000000000.50;
-     int turisticos = 30;
- 
-     printf("Estado: %c\n", Estado);
-     printf("Código: %s\n", Codigo);
-     printf("Nome: %s\n", Nome);
-     printf("População: %d de pessoas\n", Populacao);
-     printf("Área (em km²): %.2f km²\n", Area);    
-     printf("PIB: %.2f R$\n", PIB);
-     printf("Número de pontos de turísticos: %d\n", Turisticos);
+
+int main() {
+    char EstadoSP = 'A';
+    char CodigoSP[20] = "A01";
+    char NomeSP[30] = "São Paulo";
+    int PopulacaoSP = 12325000;
+    float AreaSP = 1521.11;
+    long double PIBsp = 699000000000.28;
+    int PontosTuristicosSP = 50;
+
+    char EstadoRJ = 'B';
+    char CodigoRJ[20] = "B02";
+    char NomeRJ[30] = "Rio de Janeiro";
+    int PopulacaoRJ = 6748000;
+    float AreaRJ = 1200.25;
+    long double PIBrj = 300000000000.50;
+    int PontosTuristicosRJ = 30;
+
+    // Informações da carta 1
+    printf("Estado: %c\n", EstadoSP);
+    printf("Código: %s\n", CodigoSP);
+    printf("Nome: %s\n", NomeSP);
+    printf("População: %d de pessoas\n", PopulacaoSP);
+    printf("Área (em km²): %.2f km²\n", AreaSP);    
+    printf("PIB: %.2Lf R$\n", PIBsp);
+    printf("Número de pontos de turísticos: %d\n", PontosTuristicosSP);
+    printf("\n");
  
      
-     printf("Estado: %c\n", estado);
-     printf("Código: %s\n", codigo);
-     printf("Nome: %s\n", nome);
-     printf("População: %d de pessoas\n", populacao);
-     printf("Área (em km²): %.2f km²\n", area);    
-     printf("PIB: %.2f R$\n", pib);
-     printf("Número de pontos de turísticos: %d\n", turisticos);
+    // Informações da carta 2
+    printf("Estado: %c\n", EstadoRJ);
+    printf("Código: %s\n", CodigoRJ);
+    printf("Nome: %s\n", NomeRJ);
+    printf("População: %d de pessoas\n", PopulacaoRJ);
+    printf("Área (em km²): %.2f km²\n", AreaRJ);    
+    printf("PIB: %.2Lf R$\n", PIBrj);
+    printf("Número de pontos de turísticos: %d\n", PontosTuristicosRJ);
+    printf("\n");
+
+    return 0;
  }
